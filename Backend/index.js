ad246db515c132d519b6b4fffea635d1a66938a8
Harshit Kumar <harshit892000@gmail.com>
2023-06-17 18:23:44 +0530

    second commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,15 @@
 const express= require('express');
 const mongoose= require('mongoose');
+const cors=require('cors');
 
 const app= express();
-const mongoUri='mongodb://localhost:27017/PokeDatabase';
+
+app.use(cors());
+app.use(express.json());
+
+
+const adminPassword = encodeURIComponent('Harshit123##');
+const mongoUri= `mongodb+srv://shinigami08:${adminPassword}@cluster0.9ob8dfv.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoUri,{
     useNewUrlParser:true,
@@ -16,7 +23,12 @@ mongoose.connection.on('error',(err)=>
 {
     console.log('Error connecting to Mongodb..');
 });
+// app.get('/hello', (req, res) => {
+//     console.log("fuck you");
+//     res.send({"sas":""});
+// });
 app.listen(4000,()=>
 {
     console.log("App is running on PORT 4000");
 })
+
